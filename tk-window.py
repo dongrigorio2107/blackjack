d6299d21bb2651c94289986b0959c050e84cdabb
dongrigorio2107 <grigorijspuskins@gmail.com>
2025-06-27 19:30:40 +0200

    grey squares

--- tk-window.py
+++ tk-window.py
@@ -15,16 +15,20 @@ y = (screen_height - window_height) // 2
 root.geometry(f'{window_width}x{window_height}+{x}+{y}')
 
 for i in range(3):
+    root.grid_rowconfigure(i, minsize=100)
+    root.grid_columnconfigure(i, minsize=100)
     for j in range(3):
         frame = tk.Frame(
             master=root,
+            background='grey',
             relief=tk.RAISED,
-            borderwidth=1,
-            width=10,
-            height=10
+            borderwidth=1
         )
-        frame.grid(row=i, column=j)
-        label = tk.Label(master=frame, text=f'Row {i+1}, Column {j+1}')
-        label.pack()
+        frame.grid(row=i, column=j, sticky='nsew')
+        label = tk.Label(master=frame, 
+        background='grey',
+        text=f'Row {i+1},\nColumn {j+1}'
+        )
+        label.pack(expand=True, fill='both')
 
 root.mainloop()
