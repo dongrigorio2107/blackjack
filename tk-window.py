a7b200ea258d82efa81928f052f13a8c62b26ec6
dongrigorio2107 <grigorijspuskins@gmail.com>
2025-06-28 14:53:14 +0200

    три кнопки

--- tk-window.py
+++ tk-window.py
@@ -18,44 +18,35 @@ top_frame = tk.Frame(bg="#444479", relief=tk.RAISED)
 top_frame.place(relx=0, rely=0, relheight=0.1, relwidth=1)
 
 extra_frame = tk.Frame(
-    width=200,
-    height=134,
-    borderwidth=2,
-    background='#282324',
+    width=215,
+    height=60,
+    borderwidth=3,
+    background='#287F3E',
     relief=tk.GROOVE
 )
 extra_frame.place(relx=0.5, rely=0.5, anchor='center')
 
 root_frame = tk.Frame(
-    width=300, 
-    height=300,
-    background="#282324"
+    width=200, 
+    height=50,
+    background="#287F3E"
     )
 root_frame.place(relx=0.5, rely=0.5, anchor='center')
+root_frame.grid_propagate(False)
 
 for i in range(3):
-    root.grid_rowconfigure(i, minsize=100)
-    root.grid_columnconfigure(i, minsize=100)
-    for j in range(3):
-        frame = tk.Frame(
-            master=root_frame,
-            background='grey',
-            relief=tk.RAISED,
-            borderwidth=1,
-        )
-        outer_padx = 2 if j == 0 or j == 2 else 1
-        outer_pady = 2 if i == 0 or i == 2 else 1
-
-        frame.grid(row=i,
-        column=j, 
-        sticky='nsew', 
-        padx=outer_padx,
-        pady=outer_pady
-        )
-        label = tk.Label(master=frame, 
-        background='grey',
-        text=f'Row {i},\nColumn {j}'
-        )
-        label.pack(expand=True, fill='both')
+    root_frame.columnconfigure(i, weight=1),
+    root_frame.rowconfigure(0, weight=1)
+    button = tk.Button(
+    master=root_frame,
+    background='grey',
+    activebackground='grey',
+    text=f'Row 0\nColumn {i}'
+    )
+    button.grid(
+    row=0,     
+    column=i, 
+    padx=1
+    )
 
 root.mainloop()
